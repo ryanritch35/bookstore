e3387906b898864b3d4fc73e0a19dc4164e740bd
Kyaw-Han <60949609+Kyaw-Han@users.noreply.github.com>
2022-02-26 00:07:08 -0500

    author add sql function changed, author query added

--- publishers/publisher.php
+++ publishers/publisher.php
@@ -20,6 +20,15 @@
 
     mysqli_free_result($result);
 
+    // This is for available authors
+    $sql = "SELECT DISTINCT author_name, user_id from tbl_authors ORDER BY author_name;";
+    $result = mysqli_query($conn, $sql);
+    $available_authors = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    mysqli_free_result($result);
+
+    print_r($available_authors);
+
+
     if(isset($_POST['book_add'])){
         $b_title = htmlspecialchars($_POST['book_name']);
         $b_isbn = htmlspecialchars($_POST['book_isbn']);
@@ -35,7 +44,8 @@
 
             if(mysqli_query($conn, $sql)){
                 $last_inserted_book_id = mysqli_insert_id($conn);
-                $sql = build_multi_sql_authors($last_inserted_book_id);
+                // function changed here
+                $sql = build_multi_sql_authors_id_included($last_inserted_book_id,$available_authors);
                 if(mysqli_multi_query($conn, $sql)){
                     header('refresh: 0; url = ./publisher.php');
                 } else{
@@ -66,45 +76,57 @@
     ?>
     <!-- LOL SMTHING ADDED HERE -->
     <h1>Welcome <?php echo $_SESSION['publisher_name']; ?></h1> <hr>
-    <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-        <div class="book_name">
-            <label for="book_name">Title;</label>
-            <input type="text" name="book_name" id="" placeholder="Enter Title">
-        </div>
-        <div class="book_isbn">
-            <label for="book_isbn">ISBN:</label>
-            <input type="text" name="book_isbn" id="" placeholder="Enter ISBN">
-        </div>
-        <div class="book_author">
-            <label for="book_author">Author:</label>
-            <input type="text" name="book_author" id="" placeholder="Enter Author Name">
-        </div>
-        <div class="book_genre">
-            <label for="book_genre">Genre:</label>
-            <select type="text" name="book_genre" id="" placeholder="Enter Genre">
-                <?php  foreach($book_genres as $genre): ?>
-                    <option value="<?php echo $genre; ?>"> <?php echo $genre; ?></option>
-                <?php endforeach;?>
-            </select>
-        </div>
-        <div class="book_type">
-            <label for="book_type">Type:</label>
-            <select type="text" name="book_type" id="" placeholder="Enter Book Type"> 
-                <?php $index = 0; foreach($book_types as $type): ?>
-                    <option value="<?php echo $index; ?>"> <?php echo $type; ?></option>
+    <div class="publisher-center">
+        <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+            <div class="book_name">
+                <label for="book_name">Title;</label>
+                <input type="text" name="book_name" id="" placeholder="Enter Title">
+            </div>
+            <div class="book_isbn">
+                <label for="book_isbn">ISBN:</label>
+                <input type="text" name="book_isbn" id="" placeholder="Enter ISBN">
+            </div>
+            
+            <div class="book_genre">
+                <label for="book_genre">Genre:</label>
+                <select type="text" name="book_genre" id="" placeholder="Enter Genre">
+                    <?php  foreach($book_genres as $genre): ?>
+                        <option value="<?php echo $genre; ?>"> <?php echo $genre; ?></option>
+                    <?php endforeach;?>
+                </select>
+            </div>
+            <div class="book_type">
+                <label for="book_type">Type:</label>
+                <select type="text" name="book_type" id="" placeholder="Enter Book Type"> 
+                    <?php $index = 0; foreach($book_types as $type): ?>
+                        <option value="<?php echo $index; ?>"> <?php echo $type; ?></option>
 
-                <?php $index++; endforeach;?>
-            </select>
-        </div>
-        <div class="book_price">
-            <label for="book_price">Price:</label>
-            <input type="number" name="book_price" id="" placeholder="0.00" step="0.01" min="<?php echo $min_price; ?>" max="<?php echo $max_price; ?>">
-        </div>
-        <div class="form-add">
-            <button type="submit" name="book_add" value="Add">Add</button>
-            <button type="submit" name="log_out" value="Log out">Log out</button>
-        </div>
-    </form>
+                    <?php $index++; endforeach;?>
+                </select>
+            </div>
+            <div class="book_price">
+                <label for="book_price">Price:</label>
+                <input type="number" name="book_price" id="" placeholder="0.00" step="0.01" min="<?php echo $min_price; ?>" max="<?php echo $max_price; ?>">
+            </div>
+
+            <div class="book_author">
+                <label for="book_author">Authors:</label>
+                <!-- <input type="text" name="book_author" id="" placeholder="Enter Author Name"> -->
+                <!-- <select multiple type="text" name="availabe_authors" id="" placeholder="Choose Author names"> -->
+                <div class="book_author-checkbox">
+                    <?php $index = 0; foreach($available_authors as $authors): ?>
+                        <input type="checkbox" name="availabe_authors[]" value="<?php echo $authors['author_name']; ?>"> <?php echo $authors['author_name']; ?></input><br>
+                    <?php $index++; endforeach;?>
+                <!-- </select> -->
+                </div>
+                <p>Not listed? <a href="#">Register here</a></p>
+            </div>
+            <div class="form-add">
+                <button type="submit" name="book_add" value="Add">Add</button>
+                <button type="submit" name="log_out" value="Log out">Log out</button>
+            </div>
+        </form>
+    </div>
     <hr>
     <h3>Published books</h3><br>
     <table>
