33dd12a52bc81e1d12f56ac31d0ccac1bbf8a80d
Kyaw-Han <60949609+Kyaw-Han@users.noreply.github.com>
2022-02-26 17:56:20 -0500

    vars from publisher.php moved here

--- variables/variables.php
+++ variables/variables.php
@@ -1,5 +1,9 @@
 <?php
     
+    $min_book_count = 1;
+    $max_book_count = 999;
+    $min_price = 0;
+    $max_price = 10000;
     $book_types = array("Select", "Hardcover","Paperback", "Electronic","Audio");
     $book_genres = array("Select","Action, Adventures", "Alternate History", "Anthology", "Chick Lit", "Children's", "Classic", "Comic", "Coming-of-age", "Computer Science","Crime", "Data Science","Drama", "Economics","Fairytale", "Fiction","Graphic novel", "History","Horror", "Mathematics","Mystery", "Nonfiction","Paranormal romance", "Philosophy","Picture book", "Poetry", "Political thriller", "Programming", "Psychology","Romance", "Science","Science fiction", "Short Story", "Signal Processing","Suspense", "Thriller", "Wester", "Yound adult", "Other");
 ?>
