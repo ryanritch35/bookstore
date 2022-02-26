03f76a8f94880899b651847580009c19f73e9238
Kyaw-Han <60949609+Kyaw-Han@users.noreply.github.com>
2022-02-26 17:55:57 -0500

    add book form is moved to book\add-book.php

--- publishers/publisher.php
+++ publishers/publisher.php
@@ -8,10 +8,6 @@
     $typed_inputs = array('title' =>'', 'isbn'=>'', 'genre'=>'', 'type'=>'', 'price'=>'', 'authors'=>array());
 
 
-    $min_book_count = 1;
-    $max_book_count = 999;
-    $min_price = 0;
-    $max_price = 10000;
     $publisher_name = htmlspecialchars($_SESSION['publisher_name']);
 
     $sql = "SELECT tbl_books.book_id, tbl_books.isbn, tbl_books.title, tbl_books.genre, tbl_books.book_type, tbl_books.price, tbl_books.rating, tbl_books.publisher_name FROM tbl_books WHERE publisher_name = '$publisher_name'";
@@ -113,58 +109,7 @@
     <!-- LOL SMTHING ADDED HERE -->
     <h1>Welcome <?php echo $_SESSION['publisher_name']; ?></h1> <hr>
     <div class="publisher-center">
-        <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-            <div class="book_name">
-                <label for="book_name">Title;</label>
-                <input type="text" name="book_name" id="" placeholder="Enter Title" value="<?php echo $typed_inputs['title']; ?>" required>
-            </div>
-            <div class="book_isbn">
-                <label for="book_isbn">ISBN:</label>
-                <input type="text" name="book_isbn" id="" placeholder="Enter ISBN" req<input type="text" name="book_name" id="" placeholder="Enter Title" value="<?php echo $typed_inputs['isbn']; ?>" required>
-            </div>
-            
-            <div class="book_genre">
-                <label for="book_genre">Genre:</label>
-                <select type="text" name="book_genre" id="" placeholder="Enter Genre">
-                    <?php  foreach($book_genres as $genre): ?>
-                        <option <?php if($genre == $typed_inputs['genre']) echo 'selected'; ?> value="<?php echo $genre; ?>"> <?php echo $genre; ?></option>
-                    <?php endforeach;?>
-                </select>
-            </div>
-            <div class="book_type">
-                <label for="book_type">Type:</label>
-                <select type="text" name="book_type" id="" placeholder="Enter Book Type"> 
-                    <?php $index = 0; foreach($book_types as $type): ?>
-                        <option <?php if($index == $typed_inputs['type']) echo 'selected'; ?> value="<?php echo $index; ?>"> <?php echo $type; ?></option>
-
-                    <?php $index++; endforeach;?>
-                </select>
-            </div>
-            <div class="book_price">
-                <label for="book_price">Price:</label>
-                <input type="number" name="book_price" id="" placeholder="0.00" step="0.01" min="<?php echo $min_price; ?>" max="<?php echo $max_price; ?>" value="<?php echo $typed_inputs['price']; ?>" required>
-            </div>
-
-            <div class="book_author">
-                <label for="book_author">Authors:</label>
-                <!-- <input type="text" name="book_author" id="" placeholder="Enter Author Name"> -->
-                <!-- <select multiple type="text" name="availabe_authors" id="" placeholder="Choose Author names"> -->
-                <div class="book_author-checkbox">
-                    <?php foreach($available_authors as $authors): ?>
-                        <input <?php if(array_key_exists($authors['author_name'],$typed_inputs['authors'] )) echo 'checked'; ?> type="checkbox" name="availabe_authors[]" value="<?php echo $authors['author_name']; ?>"> <?php echo $authors['author_name']; ?></input><br>
-                    <?php endforeach;?>
-                <!-- </select> -->
-                </div>
-                <p>Not listed? <a href="#">Register here</a></p>
-            </div>
-            <div class="form-add">
-                <button type="submit" name="book_add" value="Add">Add</button>  
-            </div>
-           
-        </form>
-        <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-            <button type="submit" name="log_out" value="Log out">Log out</button>
-        </form>
+        <?php include('../books/add-books.php'); ?>
     </div>
     <hr>
     <h3>Published books</h3><br>
@@ -184,7 +129,7 @@
         <?php foreach($books as $book): ?>
             <tr>
                 <td><?php echo $book['publisher_name'] ?></td>
-                <td><?php echo $book['title'] ?></td>
+                <td><a href="../books/edit-books.php?id=<?php echo $book['book_id'] ?>"><?php echo $book['title'] ?></a></td>
                 <td><?php echo $book['isbn'] ?></td>
                 <td><?php echo $book['genre'] ?></td>
                 <td><?php echo determine_Booktype($book['book_type']); ?></td>
